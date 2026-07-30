4945af0642490422f923841c9683371f66a93366
Erick Soares <erickdarochasoares@gmail.com>
2026-07-30 16:05:20 -0300

    Add files via upload

---
+++ src/main/java/com/senai/suporte/suporte/model/Solicitacao.java
@@ -0,0 +1,83 @@
+package com.senai.suporte.suporte.model;
+
+public class Solicitacao {
+    private Long id;
+    private String nif;
+    private String nomeSolicitante;
+    private String numeroSala;
+    private String codigoPatrimonio;
+    private String descricaoProblema;
+    //private StatusSolicitacao  status = new StatusSolicitacao.PENDENTE;
+
+    //Construtor
+    public Solicitacao() {
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getNomeSolicitante() {
+        return nomeSolicitante;
+    }
+
+    public void setNomeSolicitante(String nomeSolicitante) {
+        this.nomeSolicitante = nomeSolicitante;
+    }
+
+    public String getNumeroSala() {
+        return numeroSala;
+    }
+
+    public void setNumeroSala(String numeroSala) {
+        this.numeroSala = numeroSala;
+    }
+
+    public String getCodigoPatrimonio() {
+        return codigoPatrimonio;
+    }
+
+    public void setCodigoPatrimonio(String codigoPatrimonio) {
+        this.codigoPatrimonio = codigoPatrimonio;
+    }
+
+    public String getDescricaoProblema() {
+        return descricaoProblema;
+    }
+
+    public void setDescricaoProblema(String descricaoProblema) {
+        this.descricaoProblema = descricaoProblema;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Solicitacao that = (Solicitacao) obj;
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
+
